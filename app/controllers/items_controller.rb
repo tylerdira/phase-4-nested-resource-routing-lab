11a0ee229fe623be4763171c433df338fb9ee0d4
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-10-16 12:19:44 -0700

    commit tree 3705152d87578145aa7b643f98191803240a9657

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,15 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:id])
+      items = user.items
+    else 
+      items = Item.all
+    end
+    render json: items, inlude: :user
   end
+
+
 
 end
